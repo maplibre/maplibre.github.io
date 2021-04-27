512fb1af0598deea1c031aa84c8aa945eaec9414
Vaclav Janak <vaclav.janak@gmail.com>
2021-04-27 10:23:41 +0200

    Update map key

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 function mapPreview() {
   var map = new maplibregl.Map({
       container: 'map',
-      style: 'https://api.maptiler.com/maps/streets/style.json?key=5OFGN6trAtt6Hj95bXjU', // replace with correct map style and key
+      style: 'https://api.maptiler.com/maps/streets/style.json?key=ACFD57pfZ53N62uqhZ8K', // replace with correct map style
       center: [0, 0],
       zoom: 2
   });
