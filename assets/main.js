3e08326260c334df57707eaeeebcc58852243ea4
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2022-05-21 09:38:46 +0200

    Add terrain example (#40)

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,7 @@ import {
   Map,
   AttributionControl,
   NavigationControl,
+  TerrainControl,
   Marker,
 } from "maplibre-gl";
 import "bootstrap";
@@ -115,5 +116,73 @@ function mapPreview() {
     setInterval(playAnimation, lastPoint.Pause + lastPoint.Duration);
 }
 
+function terrain3dMap() {
+  var map = (window.map = new Map({
+    container: "map",
+    zoom: 12,
+    center: [11.39085, 47.27574],
+    pitch: 52,
+    hash: true,
+    style: {
+      version: 8,
+      sources: {
+        osm: {
+          type: "raster",
+          tiles: ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+          tileSize: 256,
+          attribution: "&copy; OpenStreetMap Contributors",
+          maxzoom: 19,
+        },
+        terrainSource: {
+          type: "raster-dem",
+          url: "https://demotiles.maplibre.org/terrain-tiles/tiles.json",
+          tileSize: 256,
+        },
+        hillshadeSource: {
+          type: "raster-dem",
+          url: "https://demotiles.maplibre.org/terrain-tiles/tiles.json",
+          tileSize: 256,
+        },
+      },
+      layers: [
+        {
+          id: "osm",
+          type: "raster",
+          source: "osm",
+        },
+        {
+          id: "hills",
+          type: "hillshade",
+          source: "hillshadeSource",
+          layout: { visibility: "visible" },
+          paint: { "hillshade-shadow-color": "#473B24" },
+        },
+      ],
+      terrain: {
+        source: "terrainSource",
+        exaggeration: 1,
+      },
+    },
+    maxZoom: 18,
+    maxPitch: 85,
+  }));
+
+  map.addControl(
+    new NavigationControl({
+      visualizePitch: true,
+      showZoom: true,
+      showCompass: true,
+    })
+  );
+
+  map.addControl(
+    new TerrainControl({
+      source: "terrainSource",
+      exaggeration: 1,
+    })
+  );
+}
+
 window.communityMap = communityMap;
 window.mapPreview = mapPreview;
+window.terrain3dMap = terrain3dMap;
