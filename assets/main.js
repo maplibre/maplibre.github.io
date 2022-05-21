cb0cc8fa8bebbb6476b16085badd0cfad37f969a
Maximilian Ammann <max@maxammann.org>
2022-05-21 08:42:59 +0200

    Fix formatting

--- assets/main.js
+++ assets/main.js
@@ -117,67 +117,69 @@ function mapPreview() {
 }
 
 function terrain3dMap() {
-  var map = window.map = new Map({
-    container: 'map',
+  var map = (window.map = new Map({
+    container: "map",
     zoom: 12,
     center: [11.39085, 47.27574],
     pitch: 52,
     hash: true,
     style: {
-        version: 8,
-        sources: {
-            osm: {
-                type: 'raster',
-                tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
-                tileSize: 256,
-                attribution: '&copy; OpenStreetMap Contributors',
-                maxzoom: 19
-            },
-            terrainSource: {
-                type: 'raster-dem',
-                url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-                tileSize: 256
-            },
-            hillshadeSource: {
-                type: 'raster-dem',
-                url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-                tileSize: 256
-            },
+      version: 8,
+      sources: {
+        osm: {
+          type: "raster",
+          tiles: ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+          tileSize: 256,
+          attribution: "&copy; OpenStreetMap Contributors",
+          maxzoom: 19,
         },
-        layers: [
-            {
-                id: 'osm',
-                type: 'raster',
-                source: 'osm'
-            },
-            {
-                id: 'hills',
-                type: 'hillshade',
-                source: 'hillshadeSource',
-                layout: {'visibility': 'visible'},
-                paint: {'hillshade-shadow-color': '#473B24'}
-            }
-        ],
-        terrain: {
-            source: 'terrainSource',
-            exaggeration: 1
-        }
+        terrainSource: {
+          type: "raster-dem",
+          url: "https://demotiles.maplibre.org/terrain-tiles/tiles.json",
+          tileSize: 256,
+        },
+        hillshadeSource: {
+          type: "raster-dem",
+          url: "https://demotiles.maplibre.org/terrain-tiles/tiles.json",
+          tileSize: 256,
+        },
+      },
+      layers: [
+        {
+          id: "osm",
+          type: "raster",
+          source: "osm",
+        },
+        {
+          id: "hills",
+          type: "hillshade",
+          source: "hillshadeSource",
+          layout: { visibility: "visible" },
+          paint: { "hillshade-shadow-color": "#473B24" },
+        },
+      ],
+      terrain: {
+        source: "terrainSource",
+        exaggeration: 1,
+      },
     },
     maxZoom: 18,
-    maxPitch: 85
-  });
-
-  map.addControl(new NavigationControl({
-      visualizePitch: true,
-      showZoom: true,
-      showCompass: true
+    maxPitch: 85,
   }));
 
   map.addControl(
-      new TerrainControl({
-          source: 'terrainSource',
-          exaggeration: 1
-      })
+    new NavigationControl({
+      visualizePitch: true,
+      showZoom: true,
+      showCompass: true,
+    })
+  );
+
+  map.addControl(
+    new TerrainControl({
+      source: "terrainSource",
+      exaggeration: 1,
+    })
   );
 }
 
