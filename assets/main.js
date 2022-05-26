0b480f89d9f2b23f125f0c5f95eaa72aa8dbe831
Maximilian Ammann <max@maxammann.org>
2022-05-26 16:42:03 +0200

    Fix map css

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,7 @@ import "bootstrap";
 
 function communityMap(community) {
   var map = new Map({
-    container: "map",
+    container: "community-map",
     style: "https://demotiles.maplibre.org/style.json",
     center: [0, 0],
     zoom: 1,
