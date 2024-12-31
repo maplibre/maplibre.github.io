203349df256b9dd3e8e0d7aac22fe2756dfd3002
Birk Skyum <birk.skyum@pm.me>
2024-12-31 23:14:27 +0100

    show globe on landing

--- src/components/LandingPageMap.tsx
+++ src/components/LandingPageMap.tsx
@@ -4,10 +4,19 @@ import { createEffect, onMount } from "solid-js";
 
 export const LandingPageMap = (props: any) => {
   let map;
-  onMount(() => {
+  onMount(async () => {
+  
+    const response = await fetch("https://demotiles.maplibre.org/style.json");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const style = await response.json();
+    style.projection = {type: 'globe'};
+
+
     map = new Map({
       container: "home-map",
-      style: "https://demotiles.maplibre.org/style.json",
+      style,
       center: [0, 0],
       zoom: 1,
       attributionControl: false,
@@ -20,7 +29,7 @@ export const LandingPageMap = (props: any) => {
         lng: -0.08411085041984734,
         lat: 7.115464377381386,
         Zoom: 1.3295073440521066,
-        Duration: 0,
+        Duration: 3000,
         Pause: 0,
         Rotation: 0,
         Pitch: 0,
@@ -29,7 +38,7 @@ export const LandingPageMap = (props: any) => {
         lng: 15.44385629163537,
         lat: 41.56277698075792,
         Zoom: 3.4916772106200415,
-        Duration: 5000,
+        Duration: 3000,
         Pause: 3000,
         Rotation: 0,
         Pitch: 0,
@@ -38,8 +47,8 @@ export const LandingPageMap = (props: any) => {
         lng: 5.388541216098361,
         lat: 45.57339756361293,
         Zoom: 2.15010124013107,
-        Duration: 10000,
-        Pause: 8000,
+        Duration: 3000,
+        Pause: 6000,
         Rotation: 0,
         Pitch: 0,
       },
@@ -47,8 +56,8 @@ export const LandingPageMap = (props: any) => {
         lng: -0.08411085041984734,
         lat: 7.115464377381386,
         Zoom: 1.3295073440521066,
-        Duration: 10000,
-        Pause: 8000,
+        Duration: 3000,
+        Pause: 9000,
         Rotation: 0,
         Pitch: 0,
       },
