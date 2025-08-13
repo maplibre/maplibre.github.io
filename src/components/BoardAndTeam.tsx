97b74f0e340905d6e6d0dd24358a47e83ed16741
Birk Skyum <birk.skyum@pm.me>
2025-08-13 21:31:54 +0200

    Shuffle board and team

---
+++ src/components/BoardAndTeam.tsx
@@ -0,0 +1,69 @@
+import { onMount } from "solid-js";
+import { shuffleList } from "../utils/shuffleList";
+
+export const AboutPeopleShuffle = (props: { board: any[], team: any[] }) => {
+  onMount(() => {
+    shuffleList(".board-list");
+    shuffleList(".team-list");
+  });
+
+  return (
+    <>
+      <h1 class="text-center pt-3">Governing Board</h1>
+      <div class="text-center">
+        <div class="container pb-3 px-2">
+          <div
+            class="board-list justify-content-center"
+            style="display:flex; gap: 20px; flex-wrap: wrap;"
+          >
+            {props.board.map((member) => (
+              <div class="text-center person">
+                <a href={`about/${member.link}`}>
+                  <img
+                    src={member.avatar}
+                    width="150"
+                    class="rounded-circle mt-3 border border-white"
+                  />
+                  <h3 class="m-3">{member.name}</h3>
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+        <p class="text-center">
+          In charge to steering the Organization is the
+          <a href="https://maplibre.org/about/">MapLibre Governing Board</a>. <br />
+          They are elected by
+          <a href="https://github.com/maplibre/maplibre/blob/main/VOTING_MEMBERS.md">
+            Voting Members
+          </a>
+          , a group who represents the broader community.
+        </p>
+        <div>
+          <h1 class="text-center pt-4">Team</h1>
+
+          <div class="container pb-5 pt-2">
+            <div
+              class="team-list justify-content-center"
+              style="display:flex; gap: 20px; flex-wrap: wrap;"
+            >
+              {props.team.map((member) => (
+                <div class="text-center person">
+                  <a href={`about/${member.link}`}>
+                    <img
+                      src={member.avatar}
+                      width="150"
+                      class="rounded-circle mt-3 border border-white"
+                    />
+                    <h3 class="m-3">{member.name}</h3>
+                    {member.position}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
