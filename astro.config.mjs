428a0f0f4526164bf312847e726ffe34333ff385
Bart Louwers <bart@emeel.net>
2026-01-26 16:19:34 +0100

    add link checker and make relative links absolute

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,21 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
 import icon from "astro-icon";
+import astroBrokenLinksChecker from "astro-broken-links-checker";
 
 export default defineConfig({
   base: "",
-  integrations: [mdx(), solidJs(), icon()],
+  integrations: [
+    mdx(),
+    solidJs(),
+    icon(),
+    astroBrokenLinksChecker({
+      checkExternalLinks: false, // Optional: check external links (default: false)
+      cacheExternalLinks: true, // Optional: cache verified external links to disk (default: true)
+      throwError: true, // Optional: fail the build if broken links are found (default: false)
+      linkCheckerDir: ".link-checker", // Optional: directory for cache and log files (default: '.link-checker')
+    }),
+  ],
   output: "static",
   prefetch: true,
   site: "https://maplibre.org/",
