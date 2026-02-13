43bf77130c69f271114abcfca2b6b3b6f1d82113
Birk Skyum <birk.skyum@pm.me>
2026-02-13 09:34:34 +0100

    add astro sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 import astroBrokenLinksChecker from "astro-broken-links-checker";
 
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
     mdx(),
     solidJs(),
     icon(),
+    sitemap(),
     astroBrokenLinksChecker({
       checkExternalLinks: false, // Optional: check external links (default: false)
       cacheExternalLinks: true, // Optional: cache verified external links to disk (default: true)
