239ce78be73f1dbafb52906743fc6b714aa81a38
Stephanie May <3074691+mizmay@users.noreply.github.com>
2026-06-02 09:55:20 -0700

    Add "Get Involved" section to roadmap item pages (#721)
    * feat: add help wanted section with developer and funding links to roadmap items
    * add plugin discussion and remove rounded corners funding link
    * Remove links for funding Martin
    * Restructure helpWanted frontmatter and replace OC slugs with generic OC link

--- src/content.config.ts
+++ src/content.config.ts
@@ -48,6 +48,12 @@ const roadmapItems = defineCollection({
           return new Date(`${month} 1, ${year}`);
         }),
       ),
+      links: z.optional(
+        z.object({
+          developer: z.optional(z.string().url()),
+          funding: z.optional(z.string().url()),
+        }),
+      ),
     }),
 });
 
