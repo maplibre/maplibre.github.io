506f12f220fcc4a928e1eed2a227fc6885b6e841
Bart Louwers <bart@emeel.net>
2026-08-10 22:26:30 +0200

    fix worker import MapLibre GL JS 6

--- src/components/LandingPageMap.tsx
+++ src/components/LandingPageMap.tsx
@@ -1,6 +1,6 @@
 import { Map, setWorkerUrl } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
-import workerUrl from "maplibre-gl/dist/maplibre-gl-worker?url";
+import workerUrl from "maplibre-gl/dist/maplibre-gl-worker?worker&url";
 setWorkerUrl(workerUrl);
 import { createEffect, onMount } from "solid-js";
 
