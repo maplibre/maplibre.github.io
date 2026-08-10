1d05d32658bbdb6024b3b46ee7eb670b81892ab9
Bart Louwers <bart@emeel.net>
2026-08-10 23:24:38 +0200

    fix worker import MapLibre GL JS 6 (#808)

--- src/components/CommunityMap.tsx
+++ src/components/CommunityMap.tsx
@@ -6,7 +6,7 @@ import {
   setWorkerUrl,
 } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
-import workerUrl from "maplibre-gl/dist/maplibre-gl-worker?url";
+import workerUrl from "maplibre-gl/dist/maplibre-gl-worker?worker&url";
 setWorkerUrl(workerUrl);
 import { onMount } from "solid-js";
 
--- src/content/news/2022-05-20-terrain3d/TerrainMap.tsx
+++ src/content/news/2022-05-20-terrain3d/TerrainMap.tsx
@@ -1,6 +1,6 @@
 import * as maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
-import workerUrl from "maplibre-gl/dist/maplibre-gl-worker?url";
+import workerUrl from "maplibre-gl/dist/maplibre-gl-worker?worker&url";
 maplibregl.setWorkerUrl(workerUrl);
 import { onMount } from "solid-js";
 
